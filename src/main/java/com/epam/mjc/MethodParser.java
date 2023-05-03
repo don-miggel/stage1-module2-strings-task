d96bec60067f08d2e58236ec7c895c75be3951fd
Mykhailo Derecha <don.miggel@yahoo.com>
2023-05-03 18:25:52 +0300

    strings task done

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,10 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
 public class MethodParser {
 
     /**
@@ -20,6 +25,138 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        StringBuilder sb = new StringBuilder(signatureString);
+        int posOpenBracket = signatureString.indexOf('(')+1;
+        int posClosingBracket = signatureString.indexOf(')');
+
+        String extractedArgs = signatureString.substring(posOpenBracket,posClosingBracket);
+        StringTokenizer argsToken = new StringTokenizer(extractedArgs, ",");
+        System.out.println(extractedArgs);
+        sb.delete(posOpenBracket-1, posClosingBracket+1);
+        System.out.println(sb);
+        StringTokenizer st = new StringTokenizer(sb.toString());
+        String[] parsedMethodPArts = parseMethodSignature(st);
+        System.out.println(Arrays.toString(parsedMethodPArts));
+        int countMethodTokens = parsedMethodPArts.length;
+        System.out.println(countMethodTokens);
+        MethodSignature msgt;
+        if (countMethodTokens > 2){
+            if (argsToken.countTokens() > 0)
+                msgt = new MethodSignature(parsedMethodPArts[2], parseArgs(argsToken));
+            else
+                msgt = new MethodSignature(parsedMethodPArts[2]);
+            msgt.setAccessModifier(parsedMethodPArts[0]);
+            msgt.setReturnType(parsedMethodPArts[1]);
+        }else {
+            if (argsToken.countTokens() > 0)
+                msgt = new MethodSignature(parsedMethodPArts[1], parseArgs(argsToken));
+            else
+                msgt = new MethodSignature(parsedMethodPArts[1]);
+            msgt.setReturnType(parsedMethodPArts[0]);
+        }
+
+        return msgt;
+    }
+
+    public static MethodSignature parseMthd1(String method){
+        StringBuilder sb = new StringBuilder(method);
+        int posOpenBracket = method.indexOf('(')+1;
+        int posClosingBracket = method.indexOf(')');
+
+        String extractedArgs = method.substring(posOpenBracket,posClosingBracket);
+        StringTokenizer argsToken = new StringTokenizer(extractedArgs, ",");
+        System.out.println(extractedArgs);
+        sb.delete(posOpenBracket-1, posClosingBracket+1);
+        System.out.println(sb);
+        StringTokenizer st = new StringTokenizer(sb.toString());
+        String[] parsedMethodPArts = parseMethodSignature(st);
+        System.out.println(Arrays.toString(parsedMethodPArts));
+        int countMethodTokens = parsedMethodPArts.length;
+        System.out.println(countMethodTokens);
+        MethodSignature msgt;
+        if (countMethodTokens > 2){
+            if (argsToken.countTokens() > 0)
+                msgt = new MethodSignature(parsedMethodPArts[2], parseArgs(argsToken));
+            else
+                msgt = new MethodSignature(parsedMethodPArts[2]);
+            msgt.setAccessModifier(parsedMethodPArts[0]);
+            msgt.setReturnType(parsedMethodPArts[1]);
+        }else {
+            if (argsToken.countTokens() > 0)
+                msgt = new MethodSignature(parsedMethodPArts[1], parseArgs(argsToken));
+            else
+                msgt = new MethodSignature(parsedMethodPArts[1]);
+            msgt.setReturnType(parsedMethodPArts[0]);
+        }
+
+        return msgt;
+        /*
+        System.out.println(countTokens+"countToken");
+        while (st.hasMoreTokens()) {
+            // Print all tokens
+            String parsed = st.nextToken();
+            System.out.println(parsed.trim());
+        }
+
+
+        if (countTokens > 3){
+            String[] methodParts = new String[countTokens];
+
+            MethodSignature mp = new MethodSignature(methodParts[0], null);
+        }
+
+         */
+
+    }
+    /*
+    private static MethodSignature.Argument parseArgs1(String args){
+        StringTokenizer argsToken = new StringTokenizer(args);
+        String[] parsed = new String[2];
+        while (argsToken.hasMoreTokens()) {
+            // Print all tokens
+            String ar = argsToken.nextToken();
+            System.out.println(parsed.trim());
+        }
+
+        return null;
+    }
+
+     */
+
+    private static ArrayList<MethodSignature.Argument> parseArgs(StringTokenizer args) {
+        ArrayList<MethodSignature.Argument> argsList = new ArrayList<>();
+
+        while (args.hasMoreTokens()) {
+
+            String tmp = args.nextToken();
+            StringTokenizer singeArgPair = new StringTokenizer(tmp.trim());
+            argsList.add(new MethodSignature.Argument(singeArgPair.nextToken(), singeArgPair.nextToken()));
+        }
+
+        return argsList;
+    }
+
+    private static String[] parseMethodSignature(StringTokenizer methodTokens){
+        int countTokens = methodTokens.countTokens();
+        String[] methodParts = new String[countTokens];
+        int counter = 0;
+
+        while (methodTokens.hasMoreTokens()) {
+
+            String parsed = methodTokens.nextToken();
+            methodParts[counter++] = parsed.trim();
+        }
+
+        return methodParts;
+    }
+
+        public static void main(String[] args) {
+     //       MethodSignature ms = MethodParser.parseMthd1("accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)");
+            MethodSignature ms1 = MethodParser.parseMthd1("private void log(String value)");
+            MethodSignature ms = MethodParser.parseMthd1("public DateTime getCurrentDateTime()");
+            System.out.println(ms.getAccessModifier());
+            System.out.println(ms.getMethodName());
+            System.out.println(ms.getArguments());
+            System.out.println(ms.getReturnType());
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,7 +1,9 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class StringSplitter {
 
@@ -13,6 +15,28 @@ public class StringSplitter {
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        StringBuilder sb = new StringBuilder(source);
+
+        ArrayList<String> parsedTokens = new ArrayList<>();
+
+        for (String delim : delimiters){
+            for (int i = 0; i < sb.length(); i++) {
+                if (sb.charAt(i) == delim.charAt(0))
+                    sb.replace(i, i + 1, " ");
+            }
+        }
+        StringTokenizer strt = new StringTokenizer(sb.toString());
+        while (strt.hasMoreTokens()){
+            parsedTokens.add(strt.nextToken().trim());
+        }
+
+        return parsedTokens;
+
+
+    }
+
+    public static void main(String[] args) {
+        StringSplitter sp = new StringSplitter();
+        System.out.println(sp.splitByDelimiters("qw3e1rt4yu2i3opa1sd1fg2hj4kl", List.of("1", "2", "3")));
     }
 }
